97c3e58ba91f38cce86ed3fcaf7efe41717ceaee
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-13 22:16:58 -0300

    Create MinhaPrimeiraClass.java

---
+++ aula-java/src/MinhaPrimeiraClass.java
@@ -0,0 +1,16 @@
+
+public class MinhaPrimeiraClass {
+	
+
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro projeto, olá mundo!");
+		
+		int a = 1;
+		int b = 6;
+		int c = a+b;
+		
+		System.out.println("\n" + c);
+
+	}
+
+}
