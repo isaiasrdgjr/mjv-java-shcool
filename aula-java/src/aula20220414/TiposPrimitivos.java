93c735d8d411f60bd69891be8337b22c88b8c869
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-15 10:36:56 -0300

    Update TiposPrimitivos.java

--- aula-java/src/aula20220414/TiposPrimitivos.java
+++ aula-java/src/aula20220414/TiposPrimitivos.java
@@ -3,7 +3,17 @@ package aula20220414;
 public class TiposPrimitivos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//byte vbyte = 127;
+		int vint = 1;
+		long vlong = 12345678;
+		
+		double vdouble = 12.5;
+		char vchar = 'a';
+		
+		boolean vboolean = true; //or false
+		
+		
+		//Exemplo de boolean: vboolean = 1 == 1
 
 	}
 
