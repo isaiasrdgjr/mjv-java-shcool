7130a570fef55e0c79fdcc3995bd9c70fecf73a0
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-16 15:55:38 -0300

    Create StringCriacao.java

---
+++ aula-java/src/StringCriacao.java
@@ -0,0 +1,25 @@
+       //ALGUNS EXEMPLOS
+
+public class StringCriacao {
+
+	public static void main(String[] args) {
+		String bandaX = "Coldplay";
+		String bandaY = new String ("Coldplay");
+		
+				//criando string c/ arrays de char
+		
+				char [] bandaExemplo = new char [] {'S','K','A','N','K'};
+				String banda = new String (bandaExemplo);
+		
+				System.out.println(bandaY);
+				System.out.println("\n" + banda);
+		
+		//criando string c/StringBuilder
+		
+		StringBuilder banda1 = new StringBuilder("Charlie Brown Jr");
+		String banda2 = new String (banda1);
+		
+		System.out.println("\n" + banda2);
+		
+	}
+}
