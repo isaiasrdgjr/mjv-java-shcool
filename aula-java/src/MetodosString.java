43a18861ca657b1ab4fe52c7853a496cd4b427a7
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-16 18:15:04 -0300

    Update MetodosString.java

--- aula-java/src/MetodosString.java
+++ aula-java/src/MetodosString.java
@@ -16,28 +16,28 @@ public class MetodosString {
 		System.out.println(banda1.isEmpty());// false
 		
 		//toUpperCase e toLowerCase, transformam os caracteres em maiúsculo e minusculo
-		System.out.println("\n" + banda.toUpperCase());
-		System.out.println(banda1.toLowerCase());
+		System.out.println("\n" + banda.toUpperCase()); //COLDPLAY
+		System.out.println(banda1.toLowerCase()); //skank
 		
 		//trim, tira os espaços do início e do final da String
-		System.out.println("\n" + banda2);
-		System.out.println(banda2.trim());		
+		System.out.println("\n" + banda2);//     Charlie Brown Jr
+		System.out.println(banda2.trim());//Charlie Brown Jr		
 	
 		
 		//substring, recebe um índice de começo e fim
-		System.out.println("\n" + banda1);
-		System.out.println(banda1.substring(1, 4));
+		System.out.println("\n" + banda1);// SKANK
+		System.out.println(banda1.substring(1, 4));//KAN
 	    
 		//replace, busca um caracter e substitui pelo caracter escolhido
 		System.out.println("\n" + banda2);
-		System.out.println(banda2.replace('C', 'X')); //utilizando char como parâmetro não permite a troca de 2 caracteres por 1, pois é reconhecido como um posição dentro de uma array.
-		System.out.println(banda2.replace("Ch", "X")); //utilizando string como parâmetro é possível substituir 2 caracteres por 1
+		System.out.println(banda2.replace('C', 'X')); //utilizando char como parâmetro não permite a troca de 2 caracteres por 1, pois é reconhecido como um posição dentro de uma array. = Xharlie Brown Jr
+		System.out.println(banda2.replace("Ch", "X")); //utilizando string como parâmetro é possível substituir 2 caracteres por 1 = Xarlie Brown Jr
 		
 		//equals compara se os valores da strings são iguais
-		System.out.println("\n" + banda.equals(banda1));
-		System.out.println(banda.equals("Coldplay"));
-		System.out.println(banda.equals("coldplay"));
-		System.out.println(banda.equalsIgnoreCase("Coldplay"));
+		System.out.println("\n" + banda.equals(banda1));//false
+		System.out.println(banda.equals("Coldplay"));//false
+		System.out.println(banda.equals("coldplay"));//true
+		System.out.println(banda.equalsIgnoreCase("Coldplay"));//true
 		
 		//compareTo compara se uma String "vem antes ou depois da outra" por ordem alfabética - se vier antes = valor positivo, mesma posição = 0 e valor negativo se vier depois
 		System.out.println("\n" + banda1.compareTo(banda));
