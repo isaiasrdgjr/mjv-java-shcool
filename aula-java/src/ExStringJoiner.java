536879fb02825add74f30e7470036740c7c18e44
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-18 18:11:30 -0300

    Update ExStringJoiner.java

--- aula-java/src/ExStringJoiner.java
+++ aula-java/src/ExStringJoiner.java
@@ -4,19 +4,28 @@ public class ExStringJoiner {
 
 	public static void main(String[] args) {
 		
-		StringJoiner banda = new StringJoiner(" ");
+		StringJoiner banda = new StringJoiner(" ","Rock - "," /SP - Santos");
 		
-		System.out.println(banda);
+		//System.out.println(banda);
 		banda.toString();
 		banda.add("Charlie");
 		banda.add("Brown");
 		banda.add("Junior");
 		System.out.println(banda);
 		
+		StringJoiner banda1 = new StringJoiner(" ","\nRock - "," /RJ - Rio de Janeiro");
 		
-			
+		banda1.add("e Detonautas");
+		System.out.println(banda1);
+		
+		banda.merge(banda1);
 				
-
+		StringJoiner asDuasBandas = new StringJoiner("-", "\n "," São as duas bandas");
+		asDuasBandas.merge(banda);
+		System.out.println(asDuasBandas);
+		
+				
+	
 	}
 
 }
