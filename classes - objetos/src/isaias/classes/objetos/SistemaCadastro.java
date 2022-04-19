7273fc34590c069d0d2824d8ecf1b6eebd2bace3
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-19 20:21:37 -0300

    Update SistemaCadastro.java

--- classes - objetos/src/isaias/classes/objetos/SistemaCadastro.java
+++ classes - objetos/src/isaias/classes/objetos/SistemaCadastro.java
@@ -3,8 +3,20 @@ package isaias.classes.objetos;
 public class SistemaCadastro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			Pessoa pessoa1 = new Pessoa("000001007" , "Isaias");
+			
+			System.out.println(pessoa1.endereco);
+			System.out.println(pessoa1.nome);
+			System.out.println(pessoa1.cpf 
+					+"\n");
+	
 
-	}
-
+	
+			Pessoa pessoa2 = new Pessoa("0000000012" , "Carol" , "Rio de Janeiro");
+			
+			System.out.println(pessoa2.endereco);
+			System.out.println(pessoa2.nome);
+			System.out.println(pessoa2.cpf);
+	}				
 }
