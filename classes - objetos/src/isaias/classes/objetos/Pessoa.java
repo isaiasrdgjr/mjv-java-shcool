4cdc85a4100269b7e16890b2ae1eaff1f86978d6
isaiasrdgjr <isaias.rdg@gmail.com>
2022-04-19 20:21:33 -0300

    Update Pessoa.java

--- classes - objetos/src/isaias/classes/objetos/Pessoa.java
+++ classes - objetos/src/isaias/classes/objetos/Pessoa.java
@@ -3,7 +3,20 @@ package isaias.classes.objetos;
 public class Pessoa {
 	String nome;
 	String cpf;
-	String enredeco;
+	String endereco;
 	
-
+	
+	Pessoa(String cpf, String nome, String endereco) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.endereco = endereco;
+		
+	}
+	
+	Pessoa(String cpf, String nome){
+		this(cpf, nome, "ENDEREÇO NÃO INFORMADO");
+		
+		
+	}
+	
 }
